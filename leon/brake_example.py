1c101e5826d906c6654635cd558424a6610b674c
leonkleiman1 <leonkleiman1@gmail.com>
2022-08-17 11:39:34 +0300

    hhh

---
+++ leon/brake_example.py
@@ -0,0 +1,11 @@
+count_valid=0
+grade=int(input("please enter grade:"))
+
+while grade>=0 and grade<=100:
+    count_valid+=1
+    if count_valid==5:
+        print("to many trials")
+        break
+    grade = int(input("please enter grade:"))
+else:
+    print("invalid grade")
